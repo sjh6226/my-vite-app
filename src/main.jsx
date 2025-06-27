28a3cfb6929f17790134ca8a976ad5b1ce126b4c
sjh <sjh6226@naver.com>
2025-06-27 11:53:17 +0900

    457page

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import DarkOrLight from './chapter_14/DarkOrLight.jsx'
+import Blocks from './chapter_15/Block.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <DarkOrLight />
+    {/* <DarkOrLight /> */}
+    <Blocks />
   </StrictMode>,
 )
